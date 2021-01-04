40b5ca6f046999433ecd6aadb4f1c18ac3812637
lucky rabbit <luckyrabbit870930@gmail.com>
2021-01-05 05:39:38 +0900

    Recover

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,8 @@
  *
  */
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
