55c0dcd7d2b22fb0b95d8b8750cad2b62b453261
lucky rabbit <luckyrabbit870930@gmail.com>
2021-01-09 03:58:02 +0900

    transferFrom test.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -17,6 +17,23 @@ contract("CryptoZombies", (accounts) => {
     await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
     await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {frome: alice}));
   });
+  context("with the single-step transfer scenario", async () => {
+    it("should transfer a zombie", async () => {
+      const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+      const zombieId = result.logs[0].args.zombieId.toNumber();
+      await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
+      const newOwner = await contractInstance.ownerOf(zombieId);
+      assert.equal(newOwner, bob);
+    })
+  })
+  xcontext("with the two-step transfer scenario", async () => {
+      it("should approve and then transfer a zombie when the approved address calls transferFrom", async () => {
+        // TODO: Test the two-step scenario.  The approved address calls transferFrom
+      })
+      it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
+          // TODO: Test the two-step scenario.  The owner calls transferFrom
+      })
+  })
   afterEach(async () => {
     await contractInstance.kill();
   });
